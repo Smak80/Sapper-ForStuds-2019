8eda14661015b371cfd33cc6e05a6cf0721b3843
Sergey Makletsov <smaklets@kpfu.ru>
2022-06-15 06:40:03 +0300

    Добавьте файлы проекта.

---
+++ Sapper/Form1.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Windows.Forms;
+
+namespace Sapper
+{
+    public partial class MainForm : Form
+    {
+        private Field _f;
+        private FieldController _fController;
+        public MainForm()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        private void Init()
+        {
+            if (_fController is not null)
+                _fController.Dispose();
+            _f = new Field(10, 15);
+            _fController = new FieldController(panel1, _f);
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Init();
+        }
+
+        private void panel1_Resize(object sender, EventArgs e)
+        {
+            if (_fController != null) 
+                _fController.ResizeField();
+        }
+    }
+}
